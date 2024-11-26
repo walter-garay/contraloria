ecc87f641525685f1ecb7727b1c520f90614e788
Hurosam <2021110360@udh.edu.pe>
2024-11-26 11:22:09 -0500

    Migraciones pruebas, involucrados y evaluaciones

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,11 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
+
 
 Route::middleware([
     'auth:sanctum',
